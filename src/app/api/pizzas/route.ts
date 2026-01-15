ce9469ed5a7736565c20d87c706b218684ad5aae
sdlima4025 <sergiolimafg3>
2026-01-15 19:27:29 -0300

    criado rota api/pizza.

--- src/app/api/pizzas/route.ts
+++ src/app/api/pizzas/route.ts
@@ -2,6 +2,13 @@ import { getAllProducts } from "@/services/product";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const pizzas = await getAllProducts();
+    let pizzas = await getAllProducts();
+
+    pizzas = pizzas.map(pizza => ({
+        ...pizza,
+        image: `${process.env.NEXT_PUBLIC_BASE_URL}/pizzas/${pizza.image}`
+    }));
+
+
     return  NextResponse.json({ pizzas });
-}
+    }
