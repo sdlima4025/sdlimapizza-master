d47ac1c55aa85d321b9e864772cad0d0a8c79d16
sdlima4025 <sergiolimafg3>
2026-02-25 08:12:05 -0300

    add handleMinusClick && handlePlusClick, chamando a função onClick

--- src/components/cart/cart-product.tsx
+++ src/components/cart/cart-product.tsx
@@ -6,6 +6,7 @@ import { useProducts } from "@/stores/products";
 import { CartItem } from "@/types/cart-item";
 import { Button } from "../ui/button";
 import { useState } from "react";
+import { useCart } from "@/stores/cart";
 
 type props = {
     data: CartItem;
@@ -13,10 +14,28 @@ type props = {
 
 export const CartProduct = ({ data }: props) => {
     const [qt, setQt] = useState(data.quantity);
-
+    const cart = useCart();
     const products = useProducts();
     let product = products.products.find(item => item.id === data.ProductId);
     if(!product) return null;
+
+
+
+    const handleMinusClick = () => {
+        if(qt - 1 <= 0) {
+            cart.removeItem(data.ProductId);
+
+        }else {
+            cart.addItem({ ProductId: data.ProductId, quantity: -1 });
+            setQt(qt - 1);
+        }
+    }
+
+    const handlePlusClick = () => {
+        cart.addItem({ ProductId: data.ProductId, quantity: 1 });
+        setQt(qt + 1);
+    }
+
 
     return (
         <div className="flex items-center gap-3">
@@ -34,9 +53,9 @@ export const CartProduct = ({ data }: props) => {
                 <div className="text-sm">{decimalToMoney(product.price)}</div>
             </div>
             <div className="flex items-center bg-secondary p-2 rounded-md">
-                <Button size="sm" onClick={() => {}}>-</Button>
+                <Button size="sm" onClick={handleMinusClick}>-</Button>
                 <div className="mx-3">{qt}</div>
-                <Button size="sm" onClick={() => {}}>+</Button>
+                <Button size="sm" onClick={handlePlusClick}>+</Button>
             </div>
         </div>
     );   
